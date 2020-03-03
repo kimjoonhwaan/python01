feb1811e81143763b36628866b7bd1290d2f903d
bigstart <joonhwaan@gmail.com>
2020-03-03 13:59:49 +0900

    new change01

--- google_image.py
+++ google_image.py
@@ -5,3 +5,5 @@ response = google_images_download.googleimagesdownload()   #class instantiation
 arguments = {"keywords":"Polar bears,baloons,Beaches","limit":20,"print_urls":True}   #creating list of arguments
 paths = response.download(arguments)   #passing the arguments to the function
 print(paths)   #printing absolute paths of the downloaded images
+
+#이미지 검색 오류발생
